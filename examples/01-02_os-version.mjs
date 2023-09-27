73bcf5fbb1b9283b0e9090dd12c452b8ac0e5ba5
Roberto Butti <roberto.butti@gmail.com>
2023-09-27 22:12:40 +0200

    Update 01-02_os-version.mjs

--- examples/01-02_os-version.mjs
+++ examples/01-02_os-version.mjs
@@ -1,4 +1,17 @@
-import { version } from 'os'
+import { version, platform, release, machine } from 'os'
 const v = version()
-console.log(v)
-// EXAMPLE: Darwin Kernel Version 21.6.0: Wed Aug 10 14:28:23 PDT 2022; root:xnu-8020.141.5~2/RELEASE_ARM64_T6000
+console.log(`Version: ${v}`)
+// Example
+// Version: Darwin Kernel Version 22.5.0: Thu Jun  8 22:22:20 PDT 2023; root:xnu-8796.121.3~7/RELEASE_ARM64_T6000
+const p = platform()
+console.log(`Platform: ${p}`)
+// Example
+// Platform: darwin
+const r = release()
+console.log(`Release: ${r}`)
+// Example
+// Release: 22.5.0
+const m = machine()
+console.log(`Machine: ${m}`)
+// Example
+// Machine: arm64
