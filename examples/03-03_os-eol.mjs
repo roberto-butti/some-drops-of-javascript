9efe603b8c8efec11da7ff6f4c4cae3278290377
Raghvendra Tripathi <tripathiraghvendra14@gmail.com>
2024-09-18 11:10:03 +0530

    Update to the latest version of Prettier

--- examples/03-03_os-eol.mjs
+++ examples/03-03_os-eol.mjs
@@ -1,4 +1,4 @@
-const os = require('os');
+const os = require('os')
 
-const text = 'This is the first line' + os.EOL + 'This is the second line';
-console.log(text);
+const text = 'This is the first line' + os.EOL + 'This is the second line'
+console.log(text)
