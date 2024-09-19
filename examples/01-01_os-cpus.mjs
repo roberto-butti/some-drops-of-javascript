a829a47793171a78aa384d35a8d5a4ed63eccf7a
Roberto Butti <roberto.butti@gmail.com>
2024-09-19 21:57:21 +0200

    Update 01-01_os-cpus.mjs

--- examples/01-01_os-cpus.mjs
+++ examples/01-01_os-cpus.mjs
@@ -1,5 +1,8 @@
 import { cpus } from 'os'
+
+// getting the CPUs via os.cpus() and looping
 cpus().forEach((cpu) => {
+  // each cpu item has: mode, speed and times
   console.info(cpu.model + ' speed: ' + cpu.speed)
   console.table(cpu.times)
 })
