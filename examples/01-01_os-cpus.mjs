17c9748c86cf2242d928afd25aef0f080225ffa3
Roberto Butti <roberto.butti@gmail.com>
2024-09-21 18:53:25 +0200

    Update 01-01_os-cpus.mjs

--- examples/01-01_os-cpus.mjs
+++ examples/01-01_os-cpus.mjs
@@ -1,8 +1,27 @@
 import { cpus } from 'os'
 
 // getting the CPUs via os.cpus() and looping
-cpus().forEach((cpu) => {
+cpus().forEach((cpu, index) => {
   // each cpu item has: mode, speed and times
-  console.info(cpu.model + ' speed: ' + cpu.speed)
-  console.table(cpu.times)
+  console.log(`CPU ${index + 1}:`)
+  console.log(` Model: ${cpu.model}`)
+  console.log(` Speed: ${cpu.speed} MHz`)
+  console.log(` Time (milliseconds) spent in`)
+  console.log(`  User mode      : ${cpu.times.user} ms.`)
+  console.log(`  Nice mode      : ${cpu.times.nice} ms.`)
+  console.log(`  System mode    : ${cpu.times.sys} ms.`)
+  console.log(`  For interrupts : ${cpu.times.irq} ms.`)
 })
+
+console.log('*** Calculating the CPU utilization ***')
+cpus().forEach((cpu, index) => {
+  const { user, nice, sys, idle, irq } = cpu.times
+  const total = user + nice + sys + idle + irq
+
+  const usage = ((total - idle) / total) * 100
+  console.log(`CPU ${index + 1} Usage: ${usage.toFixed(2)}%`)
+})
+
+console.log('*** Identifying the Number of Logical CPU Cores ***')
+const numCpus = cpus().length
+console.log(`Number of logical CPU cores: ${numCpus}`)
