987efcffd3064d45440a434031299c7831daef61
Roberto Butti <roberto.butti@gmail.com>
2024-09-23 08:54:36 +0200

    updating the name of the example file 02-16_array-findindex

--- examples/02-16_array-findindex.mjs
+++ examples/02-16_array-findindex.mjs
@@ -1,22 +1,22 @@
 // using findIndex() method to get the first index of the element that passes the test
-const ages = [3, 10, 18, 20];
+const ages = [3, 10, 18, 20]
 
 // storing the index value in index variable
-let index = ages.findIndex(checkAge);
+let index = ages.findIndex(checkAge)
 
 // printing the index value (3)
-console.log(index);
+console.log(index)
 
 // testing function, passed as a callback to findIndex() method
 function checkAge(age) {
-  return age > 18;
+  return age > 18
 }
 
 // findIndex() returns -1 when no element match is found
-const newAges = [3, 10, 13, 15];
+const newAges = [3, 10, 13, 15]
 
 // storing the index value in index variable
-index = newAges.findIndex(checkAge);
+index = newAges.findIndex(checkAge)
 
 // printing the index value (-1)
-console.log(index);
+console.log(index)
