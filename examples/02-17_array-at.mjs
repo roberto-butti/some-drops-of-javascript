3c91d382c07a48b94e8e65aea9f091ee8df4b1f5
Roberto Butti <roberto.butti@gmail.com>
2024-09-23 08:39:43 +0200

    Update 02-17_array-at.mjs

--- examples/02-17_array-at.mjs
+++ examples/02-17_array-at.mjs
@@ -1,18 +1,15 @@
-const fruit = ["banana", "mango", "orange", "apple"]
+const fruit = ['banana', 'mango', 'orange', 'apple']
 
 console.log(fruit.at(0)) // Output: "banana"
-
 
 const item = fruit.at(2)
 
 console.log(item) // Output: "orange"
 
-
 // Using a negative index
 
-console.log(fruit.at(-1)) // Output: "apple" 
-
+console.log(fruit.at(-1)) // Output: "apple"
 
 const item2 = fruit.at(-3)
 
-console.log(item2) // Output: "mango" 
+console.log(item2) // Output: "mango"
