cfcabb6d73dc0ba8ad6e269c09e7429b1bbfa660
Roberto Butti <roberto.butti@gmail.com>
2024-10-05 17:57:59 +0200

    formatting

--- examples/02-21_array-findlastindex.mjs
+++ examples/02-21_array-findlastindex.mjs
@@ -1,22 +1,22 @@
 // using findLastIndex() method to get the last index of the element that passes the test
-const numbers = [5, 12, 8, 130, 44];
+const numbers = [5, 12, 8, 130, 44]
 
 // storing the last index value in lastIndex variable
-let lastIndex = numbers.findLastIndex(checkLargeNumber);
+let lastIndex = numbers.findLastIndex(checkLargeNumber)
 
 // printing last index value
-console.log(lastIndex);
+console.log(lastIndex)
 
 // testing function, passed as a callback to findLastIndex() method
 function checkLargeNumber(num) {
-  return num > 10;
+  return num > 10
 }
 
 // findLastIndex() returns -1 when no element match is found
-const new_numbers = [1, 2, 3, 4, 5];
+const new_numbers = [1, 2, 3, 4, 5]
 
 // storing the last index value in lastIndex variable
-lastIndex = new_numbers.findLastIndex(checkLargeNumber);
+lastIndex = new_numbers.findLastIndex(checkLargeNumber)
 
 // printing the lastIndex variable value (-1)
-console.log(lastIndex);
+console.log(lastIndex)
--- examples/02-22_array-findlastind.mjs
+++ examples/02-22_array-findlastind.mjs
@@ -1,25 +1,25 @@
-const numbers = [5, 12, 8, 130, 44];
+const numbers = [5, 12, 8, 130, 44]
 
-const lastLargeNumber = numbers.findLast((element) => element > 10);
-console.log('Last number larger than 10:', lastLargeNumber);
+const lastLargeNumber = numbers.findLast((element) => element > 10)
+console.log('Last number larger than 10:', lastLargeNumber)
 // Expected output: 44
 
-const fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
-const lastLongFruit = fruits.findLast((fruit) => fruit.length > 5);
-console.log('Last fruit with more than 5 characters:', lastLongFruit);
+const fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+const lastLongFruit = fruits.findLast((fruit) => fruit.length > 5)
+console.log('Last fruit with more than 5 characters:', lastLongFruit)
 // Expected output: elderberry
 
 const people = [
   { name: 'Tom', age: 25 },
   { name: 'Bill', age: 30 },
   { name: 'Kate', age: 28 },
-  { name: 'Pete', age: 32 }
-];
-const lastAdult = people.findLast(person => person.age >= 30);
-console.log('Last person aged 30 or older:', lastAdult);
+  { name: 'Pete', age: 32 },
+]
+const lastAdult = people.findLast((person) => person.age >= 30)
+console.log('Last person aged 30 or older:', lastAdult)
 // Expected output: { name: 'Pete', age: 32 }
 
 // When no element satisfies the condition
-const noMatch = numbers.findLast(num => num > 1000);
-console.log('No number larger than 1000:', noMatch);
+const noMatch = numbers.findLast((num) => num > 1000)
+console.log('No number larger than 1000:', noMatch)
 // Expected output: undefined
